e014dac9cbb1e199c4d2962e7b9d3afbb09d9904
Kieran Wynne <keewynne90@gmail.com>
2022-11-18 20:22:32 +0000

    Organizes interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,14 +8,18 @@ public class Main {
     JFrame mainFrame;
     // This is a label, which will name the app
     JLabel titleLabel;
-    // This is an input box, for the user to enter a string
-    JTextField userInputField;
+    // This is an input box, for the user to enter a number
+    JTextField userInputField1;
+    // This is an input box, for the user to enter a number
+    JTextField userInputField2;
     // This is the output label, containing the response for the users string
     JLabel outputField;
     // This is the button that kicks it all in;
     JButton runButton;
     // This is a Panel, which groups together elements
     JPanel interactionPanel;
+    // Dummy component to fix span
+    JLabel __DummyLabel;
 
     public Main(){
         // I think this will run before main()
@@ -51,7 +55,8 @@ public class Main {
 
         // Set JLabel.Center below will align the component to the center of its column
         titleLabel = new JLabel("A calculator for toddler's", JLabel.CENTER);
-        userInputField = new JTextField();
+        userInputField1 = new JTextField();
+        userInputField2 = new JTextField();
         // As above set the label to be centered.
         outputField = new JLabel("",JLabel.CENTER);
         runButton = new JButton("Submit");
@@ -63,27 +68,45 @@ public class Main {
         mainFrame.add(interactionPanel);
         mainFrame.add(outputField);
 
+        __DummyLabel = new JLabel();
+
     }
 
     private void prepareInteractionPanel(){
         interactionPanel.setSize(300, 100);
-        GridLayout panelGrid = new GridLayout(2,2);
+        GridBagLayout panelGrid = new GridBagLayout();
         interactionPanel.setLayout(panelGrid);
         // Use the gbc to assign rows and columns
         GridBagConstraints gbc = new GridBagConstraints();
+        userInputField1.setSize(50,100);
+        userInputField2.setSize(50,100);
 
         // Not actually sure on the line below
-        // TODO: Get reading. GridBagContstraints.Horizontal
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        // Grid X is row number
-        gbc.gridx = 0;
-        // Grid Y is column number
-        gbc.gridy = 0;
-        // add component to panel, 2nd argument is the constraints.
-        interactionPanel.add(userInputField, gbc);
+        // TODO: Get reading. GridBagConstraints.fill:GridBagContstraints.Horizontal
+        setComponentInLayout(interactionPanel, userInputField1, 0,0,1,1,
+                GridBagConstraints.HORIZONTAL, GridBagConstraints.LINE_START, 0.5, 0.5);
 
-        gbc.gridy = 1;
-        interactionPanel.add(runButton, gbc);
+        setComponentInLayout(interactionPanel, userInputField2, 1, 0, 1,1,
+                GridBagConstraints.HORIZONTAL, GridBagConstraints.LINE_END, 0.5,0.5);
+
+        setComponentInLayout(interactionPanel,runButton,0,1,2,1,
+                GridBagConstraints.HORIZONTAL, GridBagConstraints.CENTER, 0,0);
+    }
+
+    private void setComponentInLayout(JPanel panel, JComponent component, int row, int column,
+                                      int span_columns, int span_rows, int fill, int anchor,
+                                      double weightx, double weighty){
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = fill;
+        gbc.gridy = column;
+        gbc.gridx = row;
+        gbc.gridwidth = span_columns;
+        gbc.gridheight = span_rows;
+        gbc.anchor = anchor;
+        gbc.weightx = weightx;
+        gbc.weighty = weighty;
+        panel.add(component,gbc);
+
 
     }
 }
